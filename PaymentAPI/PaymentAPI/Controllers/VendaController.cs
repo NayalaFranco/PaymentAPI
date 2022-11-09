a8ba2aadbb93f278098a0d4c80c18517008ee3fa
daniellfranco <daniel.luis.franco@gmail.com>
2022-11-08 22:04:33 -0300

    criado uma base para controller

--- PaymentAPI/PaymentAPI/Controllers/VendaController.cs
+++ PaymentAPI/PaymentAPI/Controllers/VendaController.cs
@@ -1,6 +1,39 @@
-﻿namespace PaymentAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using PaymentAPI.Context;
+using PaymentAPI.Models;
+
+namespace PaymentAPI.Controllers
 {
-    public class VendaController
+    [ApiController]
+    [Route("[controller]")]
+    public class VendaController : ControllerBase
     {
+        private readonly PaymentContext _context;
+
+        public VendaController(PaymentContext context)
+        {
+            _context = context;
+        }
+
+        //BuscarVenda, RegistrarVenda, AtualizarVenda
+        /*
+        [HttpGet("{id}")]
+        public IActionResult BuscarVenda(int id)
+        {
+            
+        }
+
+        [HttpPost]
+        public IActionResult RegistrarVenda(Venda venda)
+        {
+            
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult AtualizarVenda(int id, EnumStatusVenda status)
+        {
+            
+        }
+        */
     }
 }
