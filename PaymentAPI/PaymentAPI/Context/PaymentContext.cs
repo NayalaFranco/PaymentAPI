4e17023a4dd4351ea5d5889c7c2ca28ae5bd7e72
daniellfranco <daniel.luis.franco@gmail.com>
2022-11-08 22:48:53 -0300

    criado controlador e referenciado models na classe de Context

--- PaymentAPI/PaymentAPI/Context/PaymentContext.cs
+++ PaymentAPI/PaymentAPI/Context/PaymentContext.cs
@@ -1,6 +1,18 @@
-﻿namespace PaymentAPI.Context
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+
+namespace PaymentAPI.Context
 {
-    public class PaymentContext
+    public class PaymentContext : DbContext
     {
+        public PaymentContext(DbContextOptions<PaymentContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Venda> Vendas { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Vendedor> Vendedores { get; set; }
+
     }
 }
