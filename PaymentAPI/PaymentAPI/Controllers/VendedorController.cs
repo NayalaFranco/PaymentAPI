c2ba56355b07d81c5bc24ec145fffecdac6fc5a2
daniellfranco <daniel.luis.franco@gmail.com>
2022-11-09 23:15:48 -0300

    adicionado nova controller ainda sem metodos

---
+++ PaymentAPI/PaymentAPI/Controllers/VendedorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Context;
+using PaymentAPI.Models;
+
+namespace PaymentAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VendedorController : ControllerBase
+    {
+        //Avaliar melhor ainda se compensa fazer assim
+    }
+}
