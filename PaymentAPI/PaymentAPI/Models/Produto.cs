c4193c60decf403efabea10120a8dd1ad6dd4962
daniellfranco <daniel.luis.franco@gmail.com>
2022-11-11 22:20:26 -0300

    Alterado propriedade nome para descricao para condizer com o pedido no readme e adicionado propriedade quantidade por fazer sentido ter

--- PaymentAPI/PaymentAPI/Models/Produto.cs
+++ PaymentAPI/PaymentAPI/Models/Produto.cs
@@ -3,7 +3,8 @@
     public class Produto
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Quatidade { get; set; }
         public decimal Valor { get; set; }
     }
 }
