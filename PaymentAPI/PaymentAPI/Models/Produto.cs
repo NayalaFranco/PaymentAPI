4afdb667e10d14780ea8f0dddd57122dc3635ce3
daniellfranco <daniel.luis.franco@gmail.com>
2022-11-11 22:21:41 -0300

    corrigido nome da propriedade

--- PaymentAPI/PaymentAPI/Models/Produto.cs
+++ PaymentAPI/PaymentAPI/Models/Produto.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Descricao { get; set; }
-        public string Quatidade { get; set; }
+        public string Quantidade { get; set; }
         public decimal Valor { get; set; }
     }
 }
